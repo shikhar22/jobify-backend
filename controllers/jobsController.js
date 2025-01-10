0bd54b73743a43c67e7cdfe0ed8e75ab23e91506
ShikharTaxom <shikharag@taxomindia.in>
2025-01-10 18:43:22 +0530

    Async Error handling

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,7 +1,9 @@
 const Job = require('../models/jobs')
 const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+
 //Get All jobs => /api/v1/jobs
-exports.getJobs = async (req, res, next) => {
+exports.getJobs = catchAsyncErrors(async (req, res, next) => {
 
     const jobs = await Job.find();
     res.status(200).json({
@@ -9,10 +11,10 @@ exports.getJobs = async (req, res, next) => {
         results: jobs.length,
         data: jobs
     })
-}
+});
 
 // Get a single job with id and slug   =>  /api/v1/job/:id/:slug
-exports.getJob = (async (req, res, next) => {
+exports.getJob = catchAsyncErrors(async (req, res, next) => {
 
     const job = await Job.find({ $and: [{ _id: req.params.id }, { slug: req.params.slug }] })
     // .populate({
@@ -34,7 +36,7 @@ exports.getJob = (async (req, res, next) => {
     });
 });
 
-exports.newJob = async (req, res, next) => {
+exports.newJob = catchAsyncErrors(async (req, res, next) => {
     const job = await Job.create(req.body);
 
     res.status(200).json({
@@ -42,10 +44,10 @@ exports.newJob = async (req, res, next) => {
         message: 'Job Created',
         data: job
     })
-}
+});
 
 // Update a Job  =>  /api/v1/job/:id
-exports.updateJob = (async (req, res, next) => {
+exports.updateJob = catchAsyncErrors(async (req, res, next) => {
     let job = await Job.findById(req.params.id);
 
     if (!job) {
@@ -74,7 +76,7 @@ exports.updateJob = (async (req, res, next) => {
 });
 
 // Delete a Job   =>  /api/v1/job/:id
-exports.deleteJob = (async (req, res, next) => {
+exports.deleteJob = catchAsyncErrors(async (req, res, next) => {
     let job = await Job.findById(req.params.id).select('+applicantsApplied');
 
     if (!job) {
@@ -110,7 +112,7 @@ exports.deleteJob = (async (req, res, next) => {
 });
 
 // Get stats about a topic(job)  =>  /api/v1/stats/:topic
-exports.jobStats = (async (req, res, next) => {
+exports.jobStats = catchAsyncErrors(async (req, res, next) => {
     const stats = await Job.aggregate([
         {
             $match: { $text: { $search: "\"" + req.params.topic + "\"" } }
